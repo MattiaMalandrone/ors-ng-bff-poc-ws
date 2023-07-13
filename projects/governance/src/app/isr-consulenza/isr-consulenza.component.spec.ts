f21909f2a3b1b80cee1328b101356e86689937f1
mattia.malandrone@ors.ai <mattia.malandrone@live.com>
2023-07-13 09:06:51 +0200

    ng update @angular-eslint/schematics

--- projects/governance/src/app/isr-consulenza/isr-consulenza.component.spec.ts
+++ projects/governance/src/app/isr-consulenza/isr-consulenza.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { IsrConsulenzaComponent } from './isr-consulenza.component';
-
+// https://timdeschryver.dev/blog/testing-an-ngrx-project
 describe('IsrConsulenzaComponent', () => {
   let component: IsrConsulenzaComponent;
   let fixture: ComponentFixture<IsrConsulenzaComponent>;
